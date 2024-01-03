e45242386d3f867bacda0db97c8cedaedd881420
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-03 17:36:10 +0000

    working on counter

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <!-- <h1>Home</h1> -->
+    <div class="count">{{ count }}</div>
+    <div class="buttons">
+      <button>-</button>
+      <button @click="increaseCount">+</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+
+const increaseCount = () => {
+  console.log('Increase Count')
+  count.value++
+}
+</script>
+
+<style>
+.count {
+  font-size: 30px;
+  margin-top: 10px;
+}
+.buttons {
+  font-size: 30px;
+  /* padding: 10px; */
+}
+</style>
